7c8d283423f769d2a9786a4d64258166e1a944d2
DaniCelis <36241903+DaniCelis@users.noreply.github.com>
2018-10-13 22:18:47 -0500

    Add files via upload

---
+++ Multilistas.h
@@ -0,0 +1,208 @@
+#ifndef NOMBRE_H
+#define NOMBRE_H
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+using namespace std;
+
+template <class T>
+struct nodo{
+	T info;
+	nodo<T> *sig;
+};
+
+/*class nombre{
+	T nom;
+	nombre<T> *sig;
+	
+	public:
+		nombre(){
+			T nom = NULL;
+			sig = NULL;
+		}
+		
+	void insertar(T nomb);
+	void palabraOrden(string uno, string dos);
+	T eliminar(T nomb);
+};
+*/
+template <class T>
+class carrera{
+	nodo<T> *cab;
+	
+	public:
+		carrera(){
+			cab = new nodo<T>;
+			cab->info = NULL;
+			cab->sig=NULL;
+			 
+		}
+		
+	void insertarCarrera(T dato);
+	T retornarCarrera(int pos);
+	T eliminarCarrera(T dato);
+};
+
+template <class T>
+class hobbie{
+	nodo<T> *cab;
+	
+	public:
+		hobbie(){
+			cab = new nodo<T>;
+			cab->info = NULL;
+			cab->sig=NULL;
+			 
+		}
+		
+	void insertarHobbie(T dato);
+	T retornarHobbie(int pos);
+	T eliminarHobbie(T dato);
+};
+
+template <class T>
+class edad{
+	nodo<T> *cab;
+	
+	public:
+		edad(){
+			cab = new nodo<T>;
+			cab->info = NULL;
+			cab->sig=NULL;
+			 
+		}
+		
+	void insertarEdad(T dato);
+	T retornarEdad(int pos);
+//	T eliminarEdad(T dato);
+};
+//FUNCIONES DE CARRERA
+
+template <class T>
+void carrera<T>::insertarCarrera(T dato){
+	nodo<T> *aux, *aux1=cab;
+	aux = new nodo<T>;
+	aux->info = dato;
+	aux->sig = NULL;
+	
+	while(aux1->sig != NULL){
+		aux1 = aux1->sig;
+	}
+	
+	aux1->sig = aux;
+}
+
+template<class T>
+T carrera<T>::retornarCarrera(int pos){
+    int i;
+	nodo<T> *aux,*aux1=cab;
+	aux=new nodo<T>;
+	for(i=1;i<=pos&& aux1->sig!=NULL;i++){
+		aux1=aux1->sig;
+	}
+	return aux1->info;
+}
+
+/*template<class T>
+T carrera<T>::eliminarCarrera(T dato){
+    nodo<T> *aux,*aux1=cab;
+    aux = new nodo<T>;
+    aux->info = dato;
+    while(aux1->sig->info != dato || aux1->sig != NULL){
+    	aux1 = aux1->sig;
+	}
+	aux1->sig = aux1->sig->sig;
+	return aux->info;
+	delete aux;
+}
+*/
+
+//FUNCIONES DE HOBBIE
+
+template <class T>
+void hobbie<T>::insertarHobbie(T dato){
+	nodo<T> *aux, *aux1=cab;
+	aux = new nodo<T>;
+	aux->info = dato;
+	aux->sig = NULL;
+	
+	aux->sig = aux1->sig;
+	aux1->sig = aux;
+}
+
+template<class T>
+T hobbie<T>::retornarHobbie(int pos){
+    int i;
+	nodo<T> *aux,*aux1=cab;
+	aux=new nodo<T>;
+	for(i=1;i<=pos&& aux1->sig!=NULL;i++){
+		aux1=aux1->sig;
+	}
+	return aux1->info;
+}
+
+//FUNCIONES DE EDAD
+
+template <class T>
+void edad<T>::insertarEdad(T dato){
+	nodo<T> *aux, *aux1=cab;
+	aux = new nodo<T>;
+	aux->info = dato;
+	aux->sig = NULL;
+	
+	while(aux1->info > aux->info && aux1->sig->info > aux->info){
+		aux1 = aux1->sig; 
+	}
+	
+	aux->sig = aux1->sig;
+	aux1->sig = aux;
+}
+
+template<class T>
+T edad<T>::retornarEdad(int pos){
+    int i;
+	nodo<T> *aux,*aux1=cab;
+	aux=new nodo<T>;
+	for(i=1;i<=pos&& aux1->sig!=NULL;i++){
+		aux1=aux1->sig;
+	}
+	return aux1->info;
+}
+
+
+
+
+
+/*template<class T>
+void nombre<T>::insertar(T nomb){
+	int i;
+	nombre<T> *aux,*aux1;
+	aux= new nombre<T>;
+	aux->nom=nomb;
+	while(palabraOrden(aux1->nom, aux->nom) && aux1->sig!=NULL){
+		cout<<"mas mayor";
+	}
+	
+	aux->sig=aux1->sig;
+	aux1->sig->ant=aux;
+	
+}
+template<class T>
+bool nombre<T>::palabraOrden(string palabra1, string palabra2){
+	
+	char *nom1 = new char[palabra1.length()+1];
+	strcpy(nom1, palabra1.c_str());
+
+	
+
+//	if(strcmp(nom1,nom2)>0){
+	//	return false;
+	//}else{
+	//	return true;
+	//}
+
+}
+*/
+
+#endif
---
+++ mainmultilistas.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+#include "Nombre.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int opcionmenu=0;
+	carrera<int> Sis;
+	carrera<int> Ele;
+	carrera<int> Ind;
+	carrera<int> Cat;
+	
+	hobbie<int> Dan;
+	hobbie<int> Nat;
+	hobbie<int> Bei;
+	hobbie<int> Bas;
+	
+	edad<int> Edad;
+	
+	
+	int ingid=1;
+	string ingcarrera="";
+	string inghobbie="";
+	int ingedad = 0;
+	do{
+		
+		cout<<"Menu: \n1. Ingrese Datos \n2. Consulte Datos\n4. Eliminar Datos\n4. Terminar"<<endl;
+		cin>>opcionmenu;
+		if(opcionmenu>4){
+			cout<<"Numero Invalido"<<endl;
+		}else{
+			switch(opcionmenu){
+				case 1: 
+					
+					cout<<"Ingrese ID"<<endl;
+					cin>>ingid;
+					cout<<"Ingrese Carrera"<<endl;
+					cin>>ingcarrera;
+					
+					if(!ingcarrera.compare("Sistemas")){
+						Sis.insertarCarrera(ingid);
+					}else if(!ingcarrera.compare("Electrica")){
+						Ele.insertarCarrera(ingid);
+					}else if(!ingcarrera.compare("Industrial")){
+						Ind.insertarCarrera(ingid);
+					}else if(!ingcarrera.compare("Catastral")){
+						Cat.insertarCarrera(ingid);
+					}else{
+						cout<<"Dato incorrecto"<<endl;
+						break;
+					}
+					cout<<"Ingrese Hobbie"<<endl;
+					cin>>inghobbie;
+					if(!inghobbie.compare("Danza")){
+						Dan.insertarHobbie(ingid);
+					}else if(!inghobbie.compare("Natacion")){
+						Nat.insertarHobbie(ingid);
+					}else if(!inghobbie.compare("Beisbol")){
+						Bei.insertarHobbie(ingid);
+					}else if(!inghobbie.compare("Basket")){
+						Bas.insertarHobbie(ingid);
+					}else{
+						cout<<"Dato incorrecto"<<endl;
+						break;
+					}
+					cout<<"Ingrese Edad"<<endl;
+					cin>>ingedad;
+					Edad.insertarEdad(ingid);
+					break;
+					
+				case 2 : 
+					cout<<"Consulte Datos"<<endl;
+					//Aqui falta hacer la estructura de la consulta, pero ya estan bien
+					cout<<Dan.retornarHobbie(1)<<endl;
+					cout<<Sis.retornarCarrera(1)<<endl;
+					cout<<Edad.retornarEdad(1)<<endl;
+					
+					break;
+				case 3:
+					cout<<"Eliminar Datos"<<endl;
+					//No he implementado ninguno de eliminar porque no super como jeje
+					//cout<<Sis.eliminarCarrera(1);
+					break;
+			}
+		}
+		
+	}while(opcionmenu!=4);
+
+}
